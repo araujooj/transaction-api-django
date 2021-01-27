de40c1328223fc01b4028b4219ac831acc82a08b
Gabriel Araujo <gabriel@kenzie.com.br>
2021-01-27 09:30:20 -0300

    feature: TransactionUpdateAndDelete class, crud done

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.views import TransactionListAndCreate
+from app.views import TransactionListAndCreate, TransactionUpdateAndDelete
 
 urlpatterns = [
-    path('', TransactionListAndCreate.as_view())
+    path('', TransactionListAndCreate.as_view()),
+    path('<int:pk>', TransactionUpdateAndDelete.as_view())
 ]
--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
 from app.models import Transaction
 from app.serializers import TransactionSerializer
+from functools import reduce
 
 
 class TransactionListAndCreate(APIView):
@@ -11,12 +12,20 @@ class TransactionListAndCreate(APIView):
         transaction = Transaction.objects.all()
         serializer = TransactionSerializer(transaction, many=True)
 
+        outcome_list = [item.value for item in transaction if item.type == 'outcome']
+        outcome_value = reduce(lambda x, y: x + y, outcome_list, 0)
+
+        income_list = [item.value for item in transaction if item.type == 'income']
+        income_value = reduce(lambda x, y: x + y, income_list, 0)
+
+        total = income_value - outcome_value
+
         return Response({
             'transactions': serializer.data,
             'wallet': {
-                'income': 300,
-                'outcome': 200,
-                'total': 100
+                'income': income_value,
+                'outcome': outcome_value,
+                'total': total
             }})
 
     def post(self, request):
@@ -25,3 +34,28 @@ class TransactionListAndCreate(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TransactionUpdateAndDelete(APIView):
+    def get_object(self, pk):
+        try:
+            return Transaction.objects.get(pk=pk)
+        except Transaction.DoesNotExist:
+            raise NotFound()
+
+    def get(self, request, pk):
+        serializer = TransactionSerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = TransactionSerializer(self.get_object(pk), data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
